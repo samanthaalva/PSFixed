afd28f88ee136ae2f855177f8b8e07df9221cc22
Samantha Alva <galaxyartist2000@gmail.com>
2026-01-30 15:31:19 -0700

    Created navigation and fixed .companyMission img size in medium and small. For the most part, I think the homepage is done.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
+const btnElement = document.querySelector('#hamburgerBtn');
+const navElement = document.querySelector('#primaryNav');
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');
+    navElement.classList.toggle('active');
+})
+
+
+
 const currentYear = new Date ();
 document.querySelector('#year').textContent = currentYear.getFullYear();
